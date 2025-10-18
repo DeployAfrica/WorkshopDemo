221209ca87dc8252bd3725b17319c9fa082c9cb5
Fuad <justfuad@protonmail.com>
2025-10-18 15:29:04 +0300

    another message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def greet():
-    return "Hello, World! Welcome to deploy"
+    return "Hello, World! Welcome to deploy. From Binary labs"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
